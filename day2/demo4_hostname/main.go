bffaed4219804d72886031af81de3f9837fe0315
lichangquan <xingzhiheyi_code@163.com>
2018-01-08 22:33:41 +0800

    获取主机名和环境变量等信息

---
+++ day2/demo4_hostname/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	//获取主机名,返回字符串和error信息
+	name, ret := os.Hostname()
+	fmt.Printf("%s %v\n", name, ret) //SC-HFJS09241601 <nil>
+
+	//获得环境变量的PATH信息
+	val := os.Getenv("PATH")
+	fmt.Printf("%s\n", val)
+}
