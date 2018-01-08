bb9ba5f996e5f1f5ade7eb8372903c95d780dd17
lichangquan <xingzhiheyi_code@163.com>
2018-01-08 22:48:03 +0800

    当前时间和时间戳练习

---
+++ day2/demo7_time/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	Man    = 1
+	Female = 2
+)
+
+func main() {
+	now := time.Now()
+	second := now.Unix()
+	//2018-01-08 22:46:42.3452308 +0800 CST m=+0.011000601
+	fmt.Println(now)
+	//1515422802
+	fmt.Println(second)
+	//性别随机
+	if second%Female == 0 {
+		fmt.Println("female")
+	} else {
+		fmt.Println("man")
+	}
+}
