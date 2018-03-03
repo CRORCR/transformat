2570941af3a2f60558f1bf71ea0131602c39825e
gogogo <2926281446@qq.com>
2018-03-03 17:51:24 +0800

    commit

--- day7/demo4_bufio/bufio.go
+++ day7/demo4_bufio/bufio.go
@@ -7,7 +7,9 @@ import (
 )
 
 func main() {
+	//缓冲区读写 控制台输入信息
 	reads := bufio.NewReader(os.Stdin)
+	//按行读取  分割符号是字符,单引号  双引号是字符串
 	line, err := reads.ReadString('\n')
 	if err != nil {
 		fmt.Println("read error: ", err)
