ce33fe3f15e01d1badb18d7db64e0ca116969195
gogogo <2926281446@qq.com>
2018-04-30 21:35:16 +0800

    map反转和排序

--- day3/demo6_map/map.go
+++ day3/demo6_map/map.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 //map初始化不会分配内存,需要make来创建内存
 //map的增删改查
@@ -11,7 +14,8 @@ func main() {
 	//deleteMap(m)
 	//updateMap(m)
 	//selectMap(m)
-	seliceMap()
+	//seliceMap()
+	sortMap()
 }
 
 func addMap(m map[int]string) {
@@ -46,7 +50,44 @@ func seliceMap() {
 	fmt.Println(s)
 }
 
+func rangeMap(m map[int]string) {
+	for k, v := range m {
+		fmt.Println("\t", k, v)
+	}
+}
+
 //map排序
 //先获取所有key，把key进行排序
 //Map反转
 //初始化另外一个map，把key、value互换即可
+func sortMap(){
+	m:=make(map[int]string,3)
+	m[3]="33"
+	m[2]="22"
+	m[1]="11"
+	//1.先排序
+	var arrayList []int
+	for v:=range m{
+		arrayList=append(arrayList,v)
+	}
+	sort.Ints(arrayList)
+	//根据健找对应值
+	for _,v:=range arrayList{
+		fmt.Println(v,m[v])
+	}
+}
+
+//map反转
+func recoverMap(){
+	m:=make(map[int]string,3)
+	m[3]="33"
+	m[2]="22"
+	m[1]="11"
+
+	m2:=make(map[string]int,3)
+	for k,v:=range m{
+		m2[v]=k
+	}
+	fmt.Println(m2)
+
+}
