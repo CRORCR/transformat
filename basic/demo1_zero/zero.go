715e5e80867dc51a5b87676319da4856253f4707
gogogo <2926281446@qq.com>
2018-05-13 23:54:36 +0800

    零值 使用%q可以输出字符串""

---
+++ basic/demo1_zero/zero.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	//zero()
+	//fmt.Println(converToBin(5))
+	demo()
+}
+func zero() {
+	var a int
+	var b string
+	//fmt.Println(a, b) //0 后面空格不会输出
+	fmt.Printf("%d %q", a, b) //0 ""
+}
+
+func converToBin(n int) (result string) {
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return
+}
+
+func demo() {
+	fmt.Println(0 | 0)
+}
