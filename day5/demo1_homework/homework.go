0eb47f15a8635d2f13a684b95309358c0b5d13a2
gogogo <2926281446@qq.com>
2018-05-19 23:39:10 +0800

    快速排序

--- day5/demo1_homework/homework.go
+++ day5/demo1_homework/homework.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	a := []int{38, 1, 4, 5, 10}
+	a := []int{38, 1, 100, 5, 10}
 	//demo1(a)
 	//demo2(a)
 	//demo3(a)
@@ -55,43 +55,9 @@ func demo4(a []int) {
 		for j := i; j > 0; j-- {
 			if a[j] < a[j-1] {
 				a[j], a[j-1] = a[j-1], a[j]
+			} else {
+				break //如果发现已经放在正确的位置,就直接退出,不用比较
 			}
 		}
 	}
-}
-
-//快速排序  找到一个数,左边都比他小,右边都比他大
-//50, 100, 4, 5, 10, 50
-//10, 100, 4, 5, 38, 50
-//10, 38, 4, 5, 100, 50
-//10, 4, 38, 5, 100, 50
-//10, 4, 5, 38, 100, 50
-//5, 4, 10,
-//4, 5, 10, 38,
-func partion(a []int, left, right int) int {
-	var i = left
-	var j = right
-	for i < j {
-		for j > i && a[j] > a[left] {
-			j--
-		}
-		a[j], a[left] = a[left], a[j]
-		for i < j && a[i] < a[left] {
-			i++
-		}
-		a[left], a[i] = a[i], a[left]
-		fmt.Println(i)
-	}
-	return i
-}
-
-func qsort(a []int, left, right int) {
-	//如果左边等于右边,就是只有一个元素,不需要排序,直接返回
-	if left >= right {
-		return
-	}
-
-	mid := partion(a, left, right)
-	qsort(a, left, mid-1)
-	qsort(a, mid+1, right)
 }
