6b1207ebdc60bd933c68fd9fdeaae175246f7024
gogogo <2926281446@qq.com>
2018-06-04 17:57:32 +0800

    beego conf和log组件

--- day13/demo7_tailf/main.go
+++ day13/demo7_tailf/main.go
@@ -8,31 +8,31 @@ import (
 )
 
 func main() {
-	filename:="./my.log"
-	tails,err:=tail.TailFile(filename,tail.Config{
-		ReOpen:true,
-		Follow:true,
-		Location:&tail.SeekInfo{Offset:0,Whence:2},
-		MustExist:false,
-		Poll:true,
+	filename := "./my.log"
+	//两个参数: 读什么文件  配置
+	tails, err := tail.TailFile(filename, tail.Config{
+		ReOpen: true,
+		Follow: true,
+		//0-2 从开头读取到末尾
+		Location:  &tail.SeekInfo{Offset: 0, Whence: 2},
+		MustExist: false,
+		Poll:      true,
 	})
 
-	if err!=nil{
-		fmt.Println("tail is failed ,err ",err)
+	if err != nil {
+		fmt.Println("tail is failed ,err ", err)
 		return
 	}
 
 	var msg *tail.Line
 	var ok bool
 	for true {
-		msg,ok=<-tails.Lines
-		if !ok{
-			fmt.Printf("tail file close reopen ,filename:%s",tails.Filename)
-			time.Sleep(100*time.Millisecond)
+		msg, ok = <-tails.Lines
+		if !ok {
+			fmt.Printf("tail file close reopen ,filename:%s", tails.Filename)
+			time.Sleep(100 * time.Millisecond)
 			continue
 		}
-		fmt.Printf("msg:",msg.Text)
+		fmt.Println("msg:", msg.Text)
 	}
-
 }
-
